e038eca60919a28b3f7a4f5399ec2ffcf4655eb5
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-12-27 14:48:57 +0100

    New plants section

---
+++ plants_shop/src/components/NewPlantCard.jsx
@@ -0,0 +1,22 @@
+import "../styles/new-plants.css"
+
+export default function NewPlantCard(props) {
+    return (
+        <div className="card--wrapper">
+            <div className="plant--img">
+                <img src={`../../public/assets/${props.item.img}`} />
+            </div>
+            <div className="plant--section">
+                <div className="plant--info">
+                    <p className="plant--name">{props.item.name}</p>
+                    <p className="plant--description">{props.item.description}</p>
+                    <p className="plant--price">{props.item.price}</p>
+                </div>
+                <div className="plant--arrow">
+                    <span className="material-symbols-outlined">arrow_forward</span>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
