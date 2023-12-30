7db685c55f46f23301d7b3d628b8633439296597
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-12-30 02:21:49 +0100

    Product page design

--- plants_shop/src/pages/ProductPage.jsx
+++ plants_shop/src/pages/ProductPage.jsx
@@ -1,13 +1,26 @@
 import { useParams } from 'react-router-dom';
 import data from '../data/products'
+import Nav from '../components/Nav';
+import "../styles/product-page.css"
 
 export default function ProductPage() {
   let { id } = useParams();
   const product = data.find(p => p.id === id);
 
   return (
-    <div>
-      
-    </div>
+    <>
+        <Nav />
+        <div className="wrapper">
+            <div className="product--page--img">
+              <img src={`../src/assets/${product.img}`} />
+            </div>
+            <div className="product--page--info ">
+              <p className="product--page--name">{product.name}</p>
+              <p className="product--page--price">{product.price}</p>
+              <p className="product--page--description">{product.longDescription}</p>
+              <button className="add--cart">Add to cart</button>
+            </div>
+        </div>
+    </>
   );
 }
