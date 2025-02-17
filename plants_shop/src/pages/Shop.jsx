b320997550b2b61eadd0cacae73adc15a0a8db8c
Kamil Rudny <k.rudny1@gmail.com>
2025-02-17 22:34:03 +0100

    Shop RWD fix

--- plants_shop/src/pages/Shop.jsx
+++ plants_shop/src/pages/Shop.jsx
@@ -1,20 +1,11 @@
 import Nav from "../components/Nav"
 import Products from "../components/Products"
 import Footer from "../components/Footer"
-import { useState, useEffect } from 'react';
-import LoadingOverlay from "../components/Overlay";
 
 export default function Shop(){
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-        setIsLoading(false);
-        }, Math.floor(Math.random() * (800 - 300 + 1) + 300));
-    }, []);
 
     return (
         <>
-            <LoadingOverlay isLoading={isLoading} />
             <Nav />
             <Products />
             <Footer />
