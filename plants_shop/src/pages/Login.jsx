236d1192af0d2424bc478de71efedc203560230a
Kamil Rudny <k.rudny1@gmail.com>
2025-02-18 19:14:12 +0100

    Login and register forms

--- plants_shop/src/pages/Login.jsx
+++ plants_shop/src/pages/Login.jsx
@@ -1,25 +1,16 @@
-import Footer from "../components/Footer";
 import Nav from "../components/Nav";
-import LoadingOverlay from "../components/Overlay";
 import "../styles/contact.css";
 import "../styles/form.css";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
 
 import AuthService from "../services/AuthService";
 
 export default function Login() {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, Math.floor(Math.random() * (1000 - 500 + 1) + 500));
-    }, []);
-
+    const baseInputClass = "login--input";
+    const errorInputClass = "login--input-error";
     const navigate = useNavigate();
 
     const formik = useFormik({
@@ -29,6 +20,7 @@ export default function Login() {
         },
         validationSchema: Yup.object({
             username: Yup.string().required("This field is required!")
+                .min(6, "Username must be at least 6 characters.")
                 .max(20, "Username must be at most 20 characters."),
             password: Yup.string().required("This field is required!")
                 .min(6, "Password must be at least 6 characters.")
@@ -55,68 +47,43 @@ export default function Login() {
     });
 
     return (
-        <div className="contact--container">
-            <img className="img1" src="../../src/assets/contact2.png" />
-            <img className="img2" src="../../src/assets/contact2.png" />
-            <Nav />
-            <LoadingOverlay isLoading={isLoading} />
-            <div className="contact--wrapper">
-                <div className="contact-text">
-                    <div className="form-header">
-                        <div className="header-tab not-active">
-                            <a className="contact-1" href="/register">Sign up</a>
-                        </div>
-                        <div className="header-tab">
-                            <a className="contact-1">Log in</a>
+        <>
+            <div className="login--container">
+            <span className="white">
+                <Nav />
+            </span>
+                <div className="background--image"></div>
+                <div className="login--wrapper">
+                    <>
+                        <div className="login--tabs">
+                            <p className="sign--btn login--active">Sign in</p>
+                            <a href="/register"><p className="sign--btn login--notactive">Sign up</p></a>
                         </div>
 
-                    </div>
-                    <p className="contact-2">Are you a user?</p>
-                </div>
-                <div className="form-box flex">
-                    <form className="form flex" onSubmit={formik.handleSubmit}>
-                        <input
-                            type="text"
-                            className="input"
-                            id="username"
-                            placeholder="Your username"
-                            autoComplete="off"
-                            name="username"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.username}
-                        />
-                        {formik.touched.username && formik.errors.username && (
-                            <div className="error-message" role="alert">
-                                {formik.errors.username}
-                            </div>
-                        )}
-                        <input
-                            type="password"
-                            className="input"
-                            id="password"
-                            placeholder="Your password"
-                            autoComplete="off"
-                            name="password"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.password}
-                        />
-                        {formik.touched.password && formik.errors.password && (
-                            <div className="error-message" role="alert">
-                                {formik.errors.password}
-                            </div>
-                        )}
-                        <button className="add--cart" type="submit" disabled={formik.isSubmitting}>
-                            {formik.isSubmitting && (
-                                <span className="spinner-border spinner-border-sm"></span>
-                            )}
-                            <span>Log in</span>
-                        </button>
-                    </form>
+                        <form className="login--form" onSubmit={formik.handleSubmit}>
+                            <p className="login--text">Username</p>
+                            <input type="text" className={`${baseInputClass} ${formik.touched.username && formik.errors.username ? errorInputClass : ''}`}
+                                   id="username" placeholder="Your username" autoComplete="off" name="username"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.username}
+                            />
+                            <p className="login--text">Password</p>
+                            <input type="password" className={`${baseInputClass} ${formik.touched.password && formik.errors.password ? errorInputClass : ''}`}
+                                   id="password" placeholder="Your password" autoComplete="off" name="password"
+                                   onChange={formik.handleChange}
+                                   onBlur={formik.handleBlur}
+                                   value={formik.values.password}
+                            />
+                            <button className="login--btn" type="submit" disabled={formik.isSubmitting}>
+                                {formik.isSubmitting}
+                                Log in
+                            </button>
+
+                        </form>
+                    </>
                 </div>
             </div>
-            <Footer />
-        </div>
-    );
+        </>
+    )
 }
